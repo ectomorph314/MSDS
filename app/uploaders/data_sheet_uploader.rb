65f77553bcc05e96123c0d11c6be0accac3d5ce3
ectomorph314 <tns-GitHub@aranfell.com>
2015-04-16 15:43:24 -0400

    Refactor CarrierWave

--- app/uploaders/data_sheet_uploader.rb
+++ app/uploaders/data_sheet_uploader.rb
@@ -1,17 +1,13 @@
 # encoding: utf-8
 
 class DataSheetUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production? || Rails.env.development?
-    storage :fog
-  else
-    storage :file
-  end
+  #  storage :file
+  #  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,17 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    # region: ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory  = ENV['AWS_BUCKET']
+  if Rails.env.production? || Rails.env.development?
+    config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      # region: ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+    }
+    config.storage = :fog
+    config.fog_directory  = ENV['AWS_BUCKET']
+  else
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  end
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
