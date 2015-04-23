4323c2775cfa63cdf482daa58b171ec06e2a2d1a
ectomorph314 <tns-GitHub@aranfell.com>
2015-04-23 14:58:31 -0400

    Add Search Box

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -13,7 +13,7 @@ class CompaniesController < ApplicationController
   def show
     if CompanyUser.exists?(company_id: params[:id], user_id: current_user) || current_user.role == 'owner'
       @company = Company.find(params[:id])
-      @data_sheets = DataSheet.where(company_id: params[:id]).order(:number)
+      @data_sheets = DataSheet.search(params[:search]).where(company_id: params[:id]).order(:number)
     else
       flash[:alert] = "You don't have access to this page!"
       redirect_to root_path
